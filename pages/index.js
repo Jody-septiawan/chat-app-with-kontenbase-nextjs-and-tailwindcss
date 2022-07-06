97dae191bed883475f332e451df2fbfe7580fa4d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-07-06 09:38:47 +0700

    Creating Register Page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Register from '../components/auth/Register';
 
 export default function Auth() {
   return (
-    <div className="bg-navy">
-      <div className="container mx-auto grid grid-cols-2 gap-6 h-screen">
+    <div className="bg-navy flex flex-col">
+      <div
+        className="container mx-auto grid grid-cols-2 gap-6"
+        style={{ height: '95vh' }}
+      >
         <div className="left flex items-center">
           <div>
             <div className="text-8xl	text-white font-bold mb-5">
-              Connect <br /> to Another <br />{' '}
+              Connect <br /> to Another <br />
               <span className="text-orange">People</span>
             </div>
             <img src="/image-1.png" alt="image-1" />
@@ -18,6 +21,12 @@ export default function Auth() {
             <Register />
           </div>
         </div>
+      </div>
+      <div
+        className="footer flex justify-center items-center text-white"
+        style={{ height: '5vh' }}
+      >
+        &copy; JodySeptiawan {new Date().getFullYear()}
       </div>
     </div>
   );
